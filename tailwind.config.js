7e963de6c7ce67da45a5e1ed4fdd24dc7fb00aa9
Kawser Simanto <kawsersimanto@gmail.com>
2024-08-25 02:21:06 +0600

    created navbar with responsivness

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#adeefe",
+        primary: "#ABEF5F",
+        "primary-light": "rgba(171, 239, 95, 0.20)",
+      },
+      container: {
+        center: true,
+        padding: {
+          md: "2rem",
+          DEFAULT: "1.25rem",
+        },
+      },
+      screens: {
+        DEFAULT: "1170px",
       },
     },
   },
