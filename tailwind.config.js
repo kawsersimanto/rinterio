969b81b59a761a82f1e04577879651ff29c060fc
Kawser Simanto <kawsersimanto@gmail.com>
2024-08-25 01:28:19 +0600

    setup tailwind css instead of daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        "green": "#ABEF5F",
+        primary: "#adeefe",
       },
     },
   },
